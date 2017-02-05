5fdd641f306772ac3c1b1092ea8afa269f329435
Phil Moir <pgmoir@gmail.com>
2017-02-05 18:07:22 +0000

    Added robust platform for building forms

---
+++ src/components/common/blankPage.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var React = require('react');
+
+var Page = React.createClass({
+
+    render: function() {
+        return (
+            <div></div>
+        );
+    }
+});
+
+module.exports = Page;
